65e85371611b3706a8ef2f049d88cee54d606202
sravanidaravath <dharavathsravani55@gmail.com>
2026-08-06 10:59:49 +0530

    Add files via upload

---
+++ abstract.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+
+    def area(self):
+        return 3.14 * self.r * self.r
+
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+
+shapes = [Circle(5), Rectangle(4, 6)]
+
+for shape in shapes:
+    print("Area:", shape.area())
