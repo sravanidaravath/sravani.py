ae0caf62dc8b7b8c68f4a8663791addce1e9308e
sravanidaravath <dharavathsravani55@gmail.com>
2026-08-06 11:01:10 +0530

    Add files via upload

---
+++ runtime.py
@@ -0,0 +1,13 @@
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+a=Dog()
+b=Cat()
+a.sound()
+b.sound()
